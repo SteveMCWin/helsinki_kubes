f75d35d750c334ac24d25dfefb5f3e5f201ca46b
SteveMCWin <stevp003@gmail.com>
2025-11-14 09:43:52 +0100

    first commit, first two exercises I think

---
+++ log_output/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
+}
+
+func main() {
+	str := randSeq(10)
+	for {
+		log.Println(str)
+		time.Sleep(5 * time.Second)
+	}
+}
